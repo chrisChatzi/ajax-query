786d3cee84334e68a90df91f3e9913aa55d959ab
h2n-christos <christos.chatziioannidis@home2net.de>
2016-03-21 10:39:04 +0100

    initial commit

---
+++ ajax.js
@@ -0,0 +1,128 @@
+(function(){
+    'use strict';
+    var $ = require('jquery');
+
+    var ajaxObj = "";
+
+    var ajaxClass = function(){
+        this.url = "";
+        this.type = "";
+        this.headers = [];
+        this.data = "";
+        this.dataType = "json";
+        this.timeout = 5000;
+        this.callback = "";
+        this.errorType = false;
+        this.types = ["GET", "POST", "DELETE", "PUT"];
+        this.dataTypes = ["xml", "html", "script", "json", "jsonp", "text"];
+    }
+
+    ajaxClass.prototype = {
+        // INIT
+            init : function(url, type, headers, data, callback){
+                this.settings(url, type, headers, data, callback);
+                this.ajaxCall(this, callback);
+            },
+        // SETTINGS
+            settings : function (url, type, headers, data, callback){
+                this.setUrl(url);
+                this.setType(type, callback);
+                if(headers) this.setHeaders(headers);
+                if(data) this.setData(data);
+                this.callback = callback;
+            },
+            setUrl : function(url){
+                this.url = url;
+            },
+            setType : function(type, callback){
+                var exists = this.checkIfExists(this.types, type);
+                this.errorType = false;
+                if(exists) this.type = type;
+                else{
+                    this.errorType = true;
+                    callback("Ajax type is invalid");
+                }
+            },
+            setHeaders : function(headers){
+                this.headers = headers;
+            },
+            setData : function(data){
+                if(typeof data === 'object' || $.isPlainObject(data)) this.data = JSON.stringify(data);
+                else this.data = data;
+            },
+            setDataType : function(dataType, callback){
+                var exists = this.checkIfExists(this.dataTypes, dataType);
+                if(exists) this.dataType = dataType;
+                else callback("Data type is invalid");
+            },
+            setTimeoutVar : function(timeout, callback){
+                if(isNaN(timeout)) callback("Invalid timeout value");
+                else this.timeout = timeout;
+            },
+        // HELPERS
+            checkIfExists : function(array, value){
+                var exists = false;
+                $.map(array, function (item){
+                    if(item == value) exists = true;
+                });
+                return exists;
+            },
+        //make request
+            ajaxCall : function(self, callback){
+                if(this.errorType) return;
+                $.ajax({
+                    dataType: this.dataType,
+                    type: this.type,
+                    url: this.url,
+                    data: this.data,
+                    beforeSend: function(request){
+                        if(this.headers){
+                            $.map(this.headers, function (item){
+                                request.setRequestHeader(item.title, item.value);
+                            });
+                        }
+                    },
+                    success: function(data){
+                        data.type = "ok";
+                        self.callback(data);
+                    },
+                    error: function(xhr, textStatus, errorThrown){
+                        var data = {
+                            "type" : "error",
+                            "xhr" : xhr,
+                            "textStatus" : textStatus,
+                            "errorThrown" : errorThrown
+                        }
+                        self.callback(data);
+                    },
+                    timeout: this.timeout
+                });
+            },
+    };
+    //class calls
+    function ajaxRequest(opt, callback){
+        if(!ajaxObj) ajaxObj = new ajaxClass();
+        if(!opt.url) callback("URL cannot be empty");
+        else if(!opt.type) callback("Type cannot be empty");
+        else ajaxObj.init(opt.url, opt.type, opt.headers, opt.data, callback);
+    };
+    function ajaxSetTimeout(timeout, callback){
+        if(!ajaxObj) ajaxObj = new ajaxClass();
+        ajaxObj.setTimeoutVar(timeout, callback);
+    };
+    function ajaxSetHeaders(headers, callback){
+        if(!ajaxObj) ajaxObj = new ajaxClass();
+        ajaxObj.setHeaders(headers, callback);
+    };
+    function ajaxSetDataType(dataType, callback){
+        if(!ajaxObj) ajaxObj = new ajaxClass();
+        ajaxObj.setDataType(dataType, callback);
+    };
+    //exports
+    module.exports = {
+        ajaxRequest : ajaxRequest,
+        ajaxSetTimeout : ajaxSetTimeout,
+        ajaxSetHeaders : ajaxSetHeaders,
+        ajaxSetDataType : ajaxSetDataType
+    };
+})();
---
+++ example/index.js
@@ -0,0 +1,48 @@
+var $ = require("jquery");
+var ajax = require("../ajax");
+var template = require("./jade/main.jade");
+$('body').append(template());
+// set headers once
+ajax.ajaxSetHeaders(
+	{ "token" : "qwe"},
+	errorCallback
+);
+// set datatype once
+ajax.ajaxSetDataType(
+	"json",
+	errorCallback
+);
+// set timeout
+ajax.ajaxSetTimeout(
+	"4000", 
+	errorCallback
+);
+// a GET request
+var options = {
+	url : "https://demo.home2net.com/api/v1/device/FFFF1300FFFFFFFF5130454E2C000D4001/status/io",
+	type : "GET",
+}
+ajax.ajaxRequest(
+	options,
+	callback
+);
+// an invalid POST request
+var options = {
+	url : "https://demo.home2net.com/api/v1/device/FFFF1300FFFFFFFF5130454E2C000D4001/status/io",
+	type : "PwwOST",
+	headers : "",
+	data : {"value":"1"},
+}
+ajax.ajaxRequest(
+	options,
+	callback
+);
+// callbacks
+function errorCallback(data){
+	console.log(data);
+};
+function callback(data){
+	console.log(data);
+	if(data.type == "error") $("#result").html(data.errorThrown);
+	else $("#result").html(data);
+}
